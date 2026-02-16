a480a3a101d37dd5df86ef4c450d3a34bbb879ba
Ibrahim <Ibrahimkediramdela2211@gmail.com>
2026-02-16 17:24:11 +0300

    Add Solution class to check power of four

---
+++ power of four.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        def power(n):
+            if n == 1:
+                return True
+            if n < 1:
+                return False
+            return power(n/4)
+        return power(n)
