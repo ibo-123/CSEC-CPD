456d58f8a6d0d463810b7fd5bd27e5023ef1697b
Ibrahim Kedir <ibrahimkediramdela2211@gmail.com>
2026-02-27 23:54:51 +0300

    only plues problem is very tricky

--- A_Only_Pluses.py
+++ A_Only_Pluses.py
@@ -1,8 +1,7 @@
 for _ in range(int(input())):
         c = list(map(int, input().split()))
         c.sort()
-        
-        print(c[0]*c[1]*c[2])
-        
-        
-        
+        for i in range(5):
+                c[0] += 1
+                c.sort()
+        print(c[0]* c[1]*c[2])
--- C_Array_Splitting.py
+++ C_Array_Splitting.py
@@ -1,7 +1,7 @@
 n , m = map(int, input().split())
 arr = list(map(int, input().split()))
 if m > 1 and not m == n:
-   ans = arr[m] - arr[0]
+   ans = arr[n-m] - arr[0]
    print(ans)
 elif m  == 1:
         print(arr[n-1] - arr[0])
