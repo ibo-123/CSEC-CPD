972b59f7ff45f35508e327a6944726232f675b1e
Ibrahim Kedir <ibrahimkediramdela2211@gmail.com>
2026-03-02 11:30:52 +0300

    -y

--- A_Multiplication_Table.py
+++ A_Multiplication_Table.py
@@ -1,7 +1,11 @@
 import math
 n , m = map(int , input().split())
 coun = 0
-for i in range(2 ,int(math.ceil(n/2))+1):
+if 1 == m:
+        print(1)
+        exit()
+for i in range(2 ,n+1):
         if m % i == 0:
-                coun+=2 
+                coun+=1
+        # print(i)
 print(coun)
